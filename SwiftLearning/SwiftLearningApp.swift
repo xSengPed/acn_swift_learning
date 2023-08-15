e246a9feb5c77bd96f15dc1152c5ff2661f989eb
Donnukrit Satirakul <osacn_donnukrit.satirakul@krungthai.com>
2023-08-15 16:18:12 +0700

    Initial Commit

---
+++ SwiftLearning/SwiftLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLearningApp.swift
+//  SwiftLearning
+//
+//  Created by Donnukrit Satirakul on 15/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
